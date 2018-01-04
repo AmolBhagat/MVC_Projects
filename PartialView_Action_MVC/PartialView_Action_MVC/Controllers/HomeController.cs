fdfb9d330b9b41bbd32f1ec25c94000d4f17a4f8
AmolBhagat <amol.dhanaji.bhagat@gmail.com>
2018-01-04 13:38:31 +0530

    First Commit

---
+++ PartialView_Action_MVC/PartialView_Action_MVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialView_Action_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+            return View(from customer in entities.Customers.Take(5)
+                        select customer);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Details(string customerId)
+        {
+            NorthwindEntities entities = new NorthwindEntities();
+            return PartialView("Details", entities.Customers.Find(customerId));
+        }
+    }
+}
---
+++ PartialView_Action_MVC/PartialView_Action_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartialView_Action_MVC.MvcApplication" Language="C#" %>
